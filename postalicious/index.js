9ad607ed2f11a8105e3cb246b386a016e1dc7276
lizzkats <katsnelson.lizz@gmail.com>
2017-03-21 14:41:18 -0700

    added start scripts

---
+++ postalicious/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const server = express()
+const http = require('http').createServer(server)
+const path = require('path')
+const port = process.env.PORT || 3001
+const logger = require('morgan')
+
+server.use(logger("combined"))
+server.use(express.static(path.join(__dirname, 'public')))
+
+server.get('/', function(request, response){
+  response.send('Hello World')
+})
+
+server.set(port)
+
+http.listen(port)
+
+module.exports = server
